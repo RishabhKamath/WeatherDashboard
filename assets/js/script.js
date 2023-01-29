ca07ca2b4b42b81416139bedb2dbb4f51f4d8fe4
rishabh.kamath <rshbhk22@gmail.com>
2023-01-29 12:59:32 -0600

    moved search history container

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,12 @@
 // Get request to return lat and lon from the city search input
+
+const apiKey = "023a36acc8a97b00102fb10666b3912c";
+
+
 $(document).ready(function () {
     $("#searchButton").click(function () {
         var city = $("#cityInput").val();
-        var apiKey = "023a36acc8a97b00102fb10666b3912c";
+        // var apiKey = "023a36acc8a97b00102fb10666b3912c";
         var requestUrl = "http://api.openweathermap.org/geo/1.0/direct?q=" + city + "&appid=" + apiKey;
 
         //save search to local storage
@@ -29,7 +33,7 @@ function storeSearchInput(city) {
     var searchHistory = JSON.parse(localStorage.getItem("searchHistory")) || [];
     // Loop over search history and add each search input to the search history list
     for (var i = 0; i < searchHistory.length; i++) {
-      $("#search-history").append("<li>" + searchHistory[i] + "</li>");
+      $("#search-history").append("<li class='list-group-item'>" + searchHistory[i] + "</li>");
     }
   };
 
@@ -37,7 +41,7 @@ function storeSearchInput(city) {
 $(document).ready(function () {
     $("#searchButton").click(function () {
         var city = $("#cityInput").val();
-        var apiKey = "023a36acc8a97b00102fb10666b3912c";
+        // var apiKey = "023a36acc8a97b00102fb10666b3912c";
         var requestUrl1 = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey;
         $.get(requestUrl1, function (data) {
             var lat = data.coord.lat;
